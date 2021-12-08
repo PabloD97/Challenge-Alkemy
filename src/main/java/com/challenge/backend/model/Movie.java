a968a13dae2f30668d8adf73d2319aa408671bf8
PabloD97 <pablo97758@gmail.com>
2021-12-08 19:35:13 -0300

    feat: Movie Model
    - Added Movie Model

---
+++ src/main/java/com/challenge/backend/model/Movie.java
@@ -0,0 +1,21 @@
+package com.challenge.backend.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "films")
+public class Movie {
+
+    @Id
+    private int id;
+    private String image;
+    private String title;
+    private LocalDate creationDate;
+    @Column(length = 5)
+    private int qualification;
+    @OneToMany
+    private List<Character> characters;
+
+}
