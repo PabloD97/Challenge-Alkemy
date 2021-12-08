d12bd3cb238b0fc943876f420183e6fc90559b1c
PabloD97 <pablo97758@gmail.com>
2021-12-08 19:45:50 -0300

    feat: User Model
    - Added User model for token

---
+++ src/main/java/com/challenge/backend/model/User.java
@@ -0,0 +1,39 @@
+package com.challenge.backend.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String email;
+    private String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(){}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
