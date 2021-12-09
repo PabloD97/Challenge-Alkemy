b352ea75508b295e91d5771f6c796ebc364a3ddf
PabloD97 <pablo97758@gmail.com>
2021-12-09 11:50:17 -0300

    feat: PersonageController
    - Se agrego el endpoint '/create' para crear un Personage
    - Se agrego el endpoint '/update/id' para actualizar un Personage

--- src/main/java/com/challenge/backend/controller/PersonageController.java
+++ src/main/java/com/challenge/backend/controller/PersonageController.java
@@ -1,13 +1,13 @@
 package com.challenge.backend.controller;
 
 import com.challenge.backend.dto.PersonageDto;
+import com.challenge.backend.model.Personage;
 import com.challenge.backend.service.PersonageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +21,17 @@ public class PersonageController {
     @GetMapping("/characters")
     public ResponseEntity getAll(){
         return new ResponseEntity(personageService.getAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity createPersonage(@RequestBody Personage personage){
+        return new ResponseEntity(personageService.createPersonage(personage), HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{idPersonage}")
+    public ResponseEntity updatePersonage(
+            @PathVariable("idPersonage") int idPersonage,
+            @RequestBody Personage personage ){
+        return new ResponseEntity<>(personageService.updatePersonage(idPersonage, personage),HttpStatus.OK);
     }
 }
