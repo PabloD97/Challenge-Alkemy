3220115f8c6291ce69996b18f6d6e156f98f8d4d
PabloD97 <pablo97758@gmail.com>
2021-12-11 21:31:48 -0300

    feat: Movie Model
    - Se agregaron los metodos addPersonage() y deletePersonage()
    - Se agrego @JoinTable y CascadeType al @ManyToMany de characters
    - Se agrego el metodo movieDetails() que devuelve un string con toda la informacion de la Movie

--- src/main/java/com/challenge/backend/model/Movie.java
+++ src/main/java/com/challenge/backend/model/Movie.java
@@ -1,21 +1,70 @@
 package com.challenge.backend.model;
 
+import org.hibernate.annotations.Cascade;
+
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "films")
 public class Movie {
 
     @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String image;
     private String title;
     private LocalDate creationDate;
     @Column(length = 5)
     private int qualification;
-    @OneToMany
-    private List<Personage> characters;
 
+    @JoinTable(
+            name = "rel_films_characters",
+            joinColumns = @JoinColumn(name = "FK_FILM", nullable = false),
+            inverseJoinColumns = @JoinColumn(name="FK_PERSONAGE", nullable = false)
+    )
+    //TODO: Esto "funciona"
+    @ManyToMany(cascade = {CascadeType.PERSIST})
+    private Set<Personage> characters;
+
+    public Movie() {
+    }
+
+    public Movie(String image, String title, LocalDate creationDate, int qualification, Set<Personage> characters) {
+        this.image = image;
+        this.title = title;
+        this.creationDate = creationDate;
+        this.qualification = qualification;
+        this.characters = characters;
+    }
+
+    public void addPersonage(Personage personage) {
+        this.characters.add(personage);
+    }
+
+    public void deletePersonage(Personage personage) {
+        this.characters.remove(personage);
+    }
+
+    public String movieDetails(){
+        return "Movie{" +
+                "image:'" + image + '\'' +
+                "title:'" + title + '\'' +
+                "creation date:'" + creationDate + '\'' +
+                "qualification:'" + qualification + '\'' +
+                "characters:'" + charactersDetails() + '\'' +
+                "}";
+    }
+
+    private List<String> charactersDetails(){
+        List<String> result = new ArrayList<>();
+        this.characters.stream().forEach((personage) -> {
+            result.add(personage.getName());
+        });
+        return result;
+    }
 }
