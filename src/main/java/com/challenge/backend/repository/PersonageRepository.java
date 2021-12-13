5d8ccd17f538bd5688e72959ea94b6a8b8b1abce
PabloD97 <pablo97758@gmail.com>
2021-12-13 14:57:39 -0300

    feat: PersonageRepository
    - Se agrego la query al metodo findByName() para que la busqueda no sea tan estrictra

--- src/main/java/com/challenge/backend/repository/PersonageRepository.java
+++ src/main/java/com/challenge/backend/repository/PersonageRepository.java
@@ -1,6 +1,7 @@
 package com.challenge.backend.repository;
 
 import com.challenge.backend.model.Personage;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,8 @@ public interface PersonageRepository extends CrudRepository<Personage, Integer> 
 
     List<Personage> findAll();
     Personage findById(int id);
+
+    @Query("select p from Personage p where p.name like %?1%")
     List<Personage> findByName(String name);
 }
 
