e86622b0f812d8daae4916a97c80b3a4c33e32ba
PabloD97 <pablo97758@gmail.com>
2021-12-17 18:38:37 -0300

    feat: MovieRepository
    - Se agrego la query al metodo findByTitle() para que devuelva las peliculas que coincidan con el parametro title
    - Se agrego el metodo findByGender_Id() para buscar las peliculas que tengan ese id del genero
    - Se agregaron los metodos para devolver las listas de Movies en order Asc o Desc

--- src/main/java/com/challenge/backend/repository/MovieRepository.java
+++ src/main/java/com/challenge/backend/repository/MovieRepository.java
@@ -1,6 +1,7 @@
 package com.challenge.backend.repository;
 
 import com.challenge.backend.model.Movie;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,13 @@ public interface MovieRepository extends CrudRepository<Movie, Integer> {
 
     List<Movie> findAll();
     Movie findById(int idMovie);
+
+    @Query("select m from Movie m where m.title like %?1%")
+    List<Movie> findByTitle(String title);
+
+    List<Movie> findByGender_Id(int id);
+    List<Movie> findAllByOrderByCreationDateAsc();
+    List<Movie> findAllByOrderByCreationDateDesc();
+
+
 }
