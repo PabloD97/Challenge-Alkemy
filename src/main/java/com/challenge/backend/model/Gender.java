53be1656309f28947417178e14418ac2e144d634
PabloD97 <pablo97758@gmail.com>
2021-12-17 18:25:55 -0300

    feat: Gender Model
    - Se cambio el Set de movies a List
    - Se agrego el metodo addMovie()
    - Se agregaron los getters de los atributos name e image

--- src/main/java/com/challenge/backend/model/Gender.java
+++ src/main/java/com/challenge/backend/model/Gender.java
@@ -1,17 +1,41 @@
 package com.challenge.backend.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Table(name = "genders")
 public class Gender {
 
     @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(unique = true)
     private String name;
     private String image;
-    @ManyToMany
-    private Set<Movie> movies;
+    @OneToMany()
+    private List<Movie> movies = new ArrayList<>();
+
+    public Gender(String name, String image){
+        this.name = name;
+        this.image = image;
+    }
+
+    public Gender() {
+
+    }
+
+    public void addMovie(Movie movie){
+        this.movies.add(movie);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
 }
