25dd0d2227106bbc580d3df745cef62bbdfbea48
PabloD97 <pablo97758@gmail.com>
2021-12-27 12:29:41 -0300

    core: Data sql
    - Se agregaron inserts a la BD a traves de data.sql para generar los Generos

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Genders (
+                         id   INTEGER      NOT NULL AUTO_INCREMENT,
+                         name VARCHAR(128) NOT NULL,
+                         image VARCHAR(128) NOT NULL,
+                         PRIMARY KEY (id)
+);
+
